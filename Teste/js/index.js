64a19babf2e42b28b14f4eedda1da720a8f3f04d
kaike torres <69119354+kaiketorres@users.noreply.github.com>
2023-12-10 07:19:31 -0300

    so atualizando o teste que fico fazendo repetidamente

--- Teste/js/index.js
+++ Teste/js/index.js
@@ -1,13 +1,19 @@
-window.confirm("Esta pronto?")
+window.confirm("Está pronto?")
 
-var nome = window.prompt("Nome")
+var nome = window.prompt("Qual e seu nome?")
 
-var idade = window.prompt("idade")
+var idade = window.prompt("Quantos anos vc tem?")
 
-var n1 = Number(window.prompt("n1"))
+var dia = window.prompt("Qual dia vc nasceu?")
 
-var n2 = Number(window.prompt("n2"))
+var mes = window.prompt("Qual mes vc nasceu?")
+
+var ano = window.prompt("Qual ano vc nasceu?")
+
+var n1 = Number(window.prompt("Qual foi sua primeira nota?"))
+
+var n2 = Number(window.prompt("Qual foi sua segunda nota?"))
 
 var s = n1 + n2
 
-window.alert(`${nome} ${idade} ${s}`)
+window.alert(`Seja bem vindo ${nome} vc tem ${idade} certo, nasceu em ${dia}/${mes}/${ano} e esta cursando ads e tirou uma nota total de ${s} `)
